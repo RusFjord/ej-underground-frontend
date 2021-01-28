dd1b5a3fab067f60010d9853ad53969a847aa640
Fedor Endovitskiy <rusfjord@ulteam8.ru>
2021-01-28 18:26:04 +0300

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>EJ-Underground</h1>
+      <p>Электронный журнал для учета забросок и выбросок.</p>
+    </div>
+  );
+}
+
+export default App;
