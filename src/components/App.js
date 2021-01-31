e57267ec0325d780309422e9a90fb976f9e37b45
Fedor Endovitskiy <rusfjord@ulteam8.ru>
2021-02-01 01:13:00 +0300

    Included content and footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import Container from 'react-bootstrap/Container'
 import Header from './Header/Header'
+import Content from './Content/Content'
+import Footer from './Footer/Footer'
 
 function App() {
   return (
     <Container fluid>
       <Header />
+      <Content />
+      <Footer />
     </Container>
   )
 }
