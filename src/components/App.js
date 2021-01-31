8d3480c6b8a6ac8ac8a7d99c8090ab5fc78a752b
Fedor Endovitskiy <rusfjord@ulteam8.ru>
2021-02-01 00:15:43 +0300

    Deleted app.css and changed app.js for bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
-import './App.css'
-//const Header = require('./Header/Header')
+import Container from 'react-bootstrap/Container'
 import Header from './Header/Header' 
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <h1>EJ-Underground</h1>
-      <p>Электронный журнал для учета забросок и выбросок.</p>
-    </div>
+    <Container fluid>
+      <Container>
+        <Header />
+      </Container>
+    </Container>
   )
 }
 
