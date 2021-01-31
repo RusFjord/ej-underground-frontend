70c17bed9d3d21383942894da1189fb4c57d2958
Fedor Endovitskiy <rusfjord@ulteam8.ru>
2021-02-01 01:12:39 +0300

    Added footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import { Container } from 'react-bootstrap'
+
+import './Footer.scss'
+
+function Footer() {
+  return (
+    <Container>
+      <p>Футер</p>
+    </Container>
+  )
+}
+
+export default Footer
