802437162a5519452225816a4259fe02a7c478e5
Fedor Endovitskiy <f.endovitskiy@ulteam8.ru>
2021-02-01 13:26:09 +0300

    Added logo, title, text in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,22 @@
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
+import eujlogo from './ejulogo.png'
 import './Header.scss'
 
 function Header() {
     return (
       <header className="Header">
         <Container>
-          <h2>EJ Underground</h2>
-          <p>Электронный журнал для учета забросок и выбросок.</p>
+          <Row>
+            <Col xs lg='2'>
+              <img src={eujlogo} alt='logo'/>
+            </Col>
+            <Col className='col-site-name'>
+              <div className='site-title'>
+                <h1>EJ Underground</h1>
+                <p>Электронный журнал для учета забросок и выбросок.</p>
+              </div>         
+            </Col>
+          </Row>
         </Container>         
       </header>
     )
